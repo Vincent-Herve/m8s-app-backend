0196f9ff88d1b24a36545a0717d14435283d5742
Vincent-Herve <vincent.herve2012@laposte.net>
2020-05-28 17:20:36 +0200

    clean folder

--- frontend/src/components/Profil/Field/FieldStyled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const FieldStyled = styled.main`
-
-`;
-
-export default FieldStyled;
--- frontend/src/components/VerifyAccount/VerifyAccountStyled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const VerifyAccountStyled = styled.div`
-  padding-top: 10em;
-  min-height: 100vh;
-  text-align: center;
-`;
-
-export default VerifyAccountStyled;
